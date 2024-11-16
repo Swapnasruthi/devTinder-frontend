b653d7686bde2f0fc4a1adf8f5a9ab0a40e4e9ca
swapnasruthi <swapnasruthi2005@gmail.com>
2024-11-16 18:45:41 +0530

    redirecting to home if not authenticated

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,17 +1,45 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./NavBar"
 import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { addUser } from "./utils/userSlice";
+import { useEffect } from "react";
+import Error from "./Error";
 
 const Body = ()=> {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store) => store.user);
+    const fetchingUser = async ()=>{
+        if(userData) return userData;
+        try{
+            const res = await axios.get("http://localhost:3000/profile",
+                {withCredentials:true});
+            dispatch(addUser(res.data));
+        }
+        catch(err){
+            if(err.status === 401){
+                navigate("/login");
+
+            }
+            else{
+                {<Error/>}
+            }
+            console.error(err);
+        }
+    }
+
+    useEffect(()=>{
+        fetchingUser();
+    },[])
+
     return(
         <div className="h-screen" data-theme="dark">
             <Navbar/>
             <Outlet/>
             <Footer/>
         </div>
-        
- 
-    
     )
 }
 export default Body;
